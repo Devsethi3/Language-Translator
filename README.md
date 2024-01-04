9b62f92caa791d90f10d384e13cbb8da67ff549f
Devsethi3 <devsethi4522@gmail.com>
2024-01-04 21:54:53 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Language Translator
+
+Welcome to the Language Translator repository! This project is a JavaScript-based web application designed to translate text into multiple languages. Built with HTML, CSS, and JavaScript, it features a modern UI design for an intuitive and user-friendly experience. The translator supports copy functionality and provides voiceover for the translated text.
+
+## Preview
+
+![Language Translator Preview](Preview.png)
+
+## Features
+
+- **Multilingual Translation:** Translate text into multiple languages.
+- **Copy Functionality:** Easily copy the translated text to the clipboard.
+- **Voiceover:** Listen to the pronunciation of the translated text.
+- **Modern UI Design:** Sleek layout and visually appealing design for an immersive translation experience.
+
+## Getting Started
+
+To run the Language Translator locally, follow these steps:
+
+1. Clone the repository to your local machine:
+
+   ```bash
+   git clone https://github.com/Devsethi3/Language-Translator.git
+   ```
+
+2. Open the `index.html` file in your preferred web browser.
+
+## Usage
+
+1. Enter the text you want to translate into the input area.
+2. Select the target language from the dropdown menu.
+3. Click the "Translate" button to see the translated text.
+4. Use the "Copy" button to copy the translated text to the clipboard.
+5. Click the speaker icon to listen to the pronunciation.
+
+## Customization
+
+Feel free to customize this project to fit your preferences. Update styles, colors, and layout in the HTML and CSS files. You can also extend the functionality to include additional features or support more languages.
+
+## Contributing
+
+If you'd like to contribute to this project, please follow these steps:
+
+1. Fork the repository.
+2. Create a new branch for your feature or improvement.
+3. Make your changes and commit them with descriptive messages.
+4. Push your changes to your forked repository.
+5. Open a pull request to merge your changes into the main branch.
+
+Explore the Language Translator, break down language barriers, and consider contributing to its development. Thank you for checking out the repository!
---
+++ script.js
@@ -0,0 +1,182 @@
+const countries = {
+  "am-ET": "Amharic",
+  "ar-SA": "Arabic",
+  "be-BY": "Bielarus",
+  "bem-ZM": "Bemba",
+  "bi-VU": "Bislama",
+  "bjs-BB": "Bajan",
+  "bn-IN": "Bengali",
+  "bo-CN": "Tibetan",
+  "br-FR": "Breton",
+  "bs-BA": "Bosnian",
+  "ca-ES": "Catalan",
+  "cop-EG": "Coptic",
+  "cs-CZ": "Czech",
+  "cy-GB": "Welsh",
+  "da-DK": "Danish",
+  "dz-BT": "Dzongkha",
+  "de-DE": "German",
+  "dv-MV": "Maldivian",
+  "el-GR": "Greek",
+  "en-GB": "English",
+  "es-ES": "Spanish",
+  "et-EE": "Estonian",
+  "eu-ES": "Basque",
+  "fa-IR": "Persian",
+  "fi-FI": "Finnish",
+  "fn-FNG": "Fanagalo",
+  "fo-FO": "Faroese",
+  "fr-FR": "French",
+  "gl-ES": "Galician",
+  "gu-IN": "Gujarati",
+  "ha-NE": "Hausa",
+  "he-IL": "Hebrew",
+  "hi-IN": "Hindi",
+  "hr-HR": "Croatian",
+  "hu-HU": "Hungarian",
+  "id-ID": "Indonesian",
+  "is-IS": "Icelandic",
+  "it-IT": "Italian",
+  "ja-JP": "Japanese",
+  "kk-KZ": "Kazakh",
+  "km-KM": "Khmer",
+  "kn-IN": "Kannada",
+  "ko-KR": "Korean",
+  "ku-TR": "Kurdish",
+  "ky-KG": "Kyrgyz",
+  "la-VA": "Latin",
+  "lo-LA": "Lao",
+  "lv-LV": "Latvian",
+  "men-SL": "Mende",
+  "mg-MG": "Malagasy",
+  "mi-NZ": "Maori",
+  "ms-MY": "Malay",
+  "mt-MT": "Maltese",
+  "my-MM": "Burmese",
+  "ne-NP": "Nepali",
+  "niu-NU": "Niuean",
+  "nl-NL": "Dutch",
+  "no-NO": "Norwegian",
+  "ny-MW": "Nyanja",
+  "ur-PK": "Pakistani",
+  "pau-PW": "Palauan",
+  "pa-IN": "Panjabi",
+  "ps-PK": "Pashto",
+  "pis-SB": "Pijin",
+  "pl-PL": "Polish",
+  "pt-PT": "Portuguese",
+  "rn-BI": "Kirundi",
+  "ro-RO": "Romanian",
+  "ru-RU": "Russian",
+  "sg-CF": "Sango",
+  "si-LK": "Sinhala",
+  "sk-SK": "Slovak",
+  "sm-WS": "Samoan",
+  "sn-ZW": "Shona",
+  "so-SO": "Somali",
+  "sq-AL": "Albanian",
+  "sr-RS": "Serbian",
+  "sv-SE": "Swedish",
+  "sw-SZ": "Swahili",
+  "ta-LK": "Tamil",
+  "te-IN": "Telugu",
+  "tet-TL": "Tetum",
+  "tg-TJ": "Tajik",
+  "th-TH": "Thai",
+  "ti-TI": "Tigrinya",
+  "tk-TM": "Turkmen",
+  "tl-PH": "Tagalog",
+  "tn-BW": "Tswana",
+  "to-TO": "Tongan",
+  "tr-TR": "Turkish",
+  "uk-UA": "Ukrainian",
+  "uz-UZ": "Uzbek",
+  "vi-VN": "Vietnamese",
+  "wo-SN": "Wolof",
+  "xh-ZA": "Xhosa",
+  "yi-YD": "Yiddish",
+  "zu-ZA": "Zulu",
+};
+
+const selectTag = document.querySelectorAll("select");
+const icons = document.querySelectorAll(".row i");
+const fromText = document.querySelector(".from-text");
+const toText = document.querySelector(".to-text");
+const exchangeIcon = document.querySelector(".exchange");
+const translateBtn = document.querySelector("button");
+
+selectTag.forEach((selectTag, id) => {
+  for (const country_code in countries) {
+    let selected =
+      (id == 0 && country_code == "en-GB") ||
+      (id == 1 && country_code == "hi-IN")
+        ? "selected"
+        : "";
+    let option = `<option value="${country_code}" ${selected}>${countries[country_code]}</option>`;
+    selectTag.insertAdjacentHTML("beforeend", option);
+  }
+});
+
+translateBtn.addEventListener("click", () => {
+  let text = fromText.value;
+  let translateFrom = selectTag[0].value;
+  let translateTo = selectTag[1].value;
+
+  if (!text) {
+    alert("Please enter text to translate.");
+    return;
+  }
+
+  toText.setAttribute("placeholder", "Translating...");
+
+  let url = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.responseStatus === 200) {
+        toText.value = data.responseData.translatedText;
+      } else {
+        alert("Translation failed. Please try again.");
+      }
+    })
+    .catch((error) => {
+      alert("An error occurred. Please try again later.");
+      toText.removeAttribute("placeholder");
+    });
+});
+
+exchangeIcon.addEventListener("click", () => {
+  let tempText = fromText.value,
+    tempLang = selectTag[0].value;
+  fromText.value = toText.value;
+  selectTag[0].value = selectTag[1].value;
+  toText.value = tempText;
+  selectTag[1].value = tempLang;
+});
+
+icons.forEach((icon) => {
+  icon.addEventListener("click", ({ target }) => {
+    if (target.classList.contains("fa-copy")) {
+      const tooltip = document.querySelector(".tooltip");
+      tooltip.classList.add("show-tooltip");
+      setTimeout(() => {
+        tooltip.classList.remove("show-tooltip");
+      }, 1000);
+      if (target.id == "from") {
+        navigator.clipboard.writeText(fromText.value);
+      } else {
+        navigator.clipboard.writeText(toText.value);
+      }
+    } else {
+      let utterance;
+      if (target.id == "from") {
+        utterance = new SpeechSynthesisUtterance(fromText.value);
+        utterance.lang = selectTag[0].value;
+      } else {
+        utterance = new SpeechSynthesisUtterance(toText.value);
+        utterance.lang = selectTag[1].value;
+      }
+      speechSynthesis.speak(utterance);
+    }
+  });
+});
